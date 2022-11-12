85083da164bafa4e010de5358ba33ff6cf528f99
Görkem YAVUZ <gorkem.yavuz.39@hotmail.com>
2022-11-12 11:51:40 +0300

    Yetkinlik kontratının testlerine başlandı.

--- test/test.js
+++ test/test.js
@@ -5,8 +5,18 @@ const provider = ethers.provider;
 
 describe("Test hesaplarının oluşturulması", function () {
 
-    it("Adreslerin oluşturulması ve kontrolü", async function() {
-        const [devlet, kamuKurumu1, kamuKurumu2, kamuKurumu3, firma1, firma2, firma3, misafir1, misafir2] = await ethers.getSigners();
+    let devlet, kamuKurumu1, kamuKurumu2, kamuKurumu3, firma1, firma2, firma3, misafir1, misafir2;
+    let competenceC;
+
+    before(async function() {
+        [devlet, kamuKurumu1, kamuKurumu2, kamuKurumu3, firma1, firma2, firma3, misafir1, misafir2] = await ethers.getSigners();
+
+        competenceContract = await ethers.getContractFactory("EKAPB_COMPETENCE");
+        competenceC = await competenceContract.connect(devlet).deploy();
+        
+    });
+
+    it("Hesapların ve kontratların oluşturulup oluşturulmadığı kontrolü", async function() {
         expect(devlet.address).to.not.be.undefined;
         expect(kamuKurumu1.address).to.not.be.undefined;
         expect(kamuKurumu2.address).to.not.be.undefined;
@@ -16,12 +26,9 @@ describe("Test hesaplarının oluşturulması", function () {
         expect(firma3.address).to.not.be.undefined;
         expect(misafir1.address).to.not.be.undefined;
         expect(misafir2.address).to.not.be.undefined;
+
+        expect(competenceC.address).to.not.be.undefined;
     });
 
-    
 
 });
-/*describe("Yetkinlik kontratı", async function() {
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-});*/
