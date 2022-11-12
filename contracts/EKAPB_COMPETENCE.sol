21f76014f82a89decb8555735670f072fa991335
Görkem YAVUZ <gorkem.yavuz.39@hotmail.com>
2022-11-12 10:45:05 +0300

    Yetkinlik kontratı hazırlandı

--- contracts/EKAPB_COMPETENCE.sol
+++ contracts/EKAPB_COMPETENCE.sol
@@ -2,5 +2,27 @@
 pragma solidity ^0.8.4;
 
 contract EKAPB_COMPETENCE {
+
+    address creator;
+
+    // Address => Competence => True & False
+    mapping(address => mapping(uint256 => bool)) competence;
+
+    modifier onlyOwner {
+        require(msg.sender == creator, "Bu yetkiye sahip degilsiniz.");
+        _;
+    }
+
+    constructor() {
+        creator = msg.sender;
+    }
+
+    function getCompetence(address _address, uint256 _competenceID) public view returns(bool) {
+        return competence[_address][_competenceID];
+    }
+
+    function setCompetence(address _address, uint256 _competenceID) public onlyOwner {
+        competence[_address][_competenceID] = true;
+    }
     
 }
